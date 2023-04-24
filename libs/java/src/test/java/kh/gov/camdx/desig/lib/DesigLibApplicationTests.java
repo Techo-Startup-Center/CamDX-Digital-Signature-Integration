67aae6d1a222bbcf498bcce33d06d16355bfb370
sereyvathanatum <sereyvathana.tum@techostartup.center>
2023-04-24 17:11:41 +0700

    first commit

---
+++ libs/java/src/test/java/kh/gov/camdx/desig/lib/DesigLibApplicationTests.java
@@ -0,0 +1,13 @@
+package kh.gov.camdx.desig.lib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesigLibApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
