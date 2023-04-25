0e1b5a57302982f55879640ffa1667c2b96c325f
sereyvathana <sereyvathana.tum@techostartup.center>
2023-04-25 11:21:59 +0700

    start wrapper-services

---
+++ wrapper-service/src/main/java/kh/gov/camdx/disig/wrapperservice/WrapperServiceApplication.java
@@ -0,0 +1,13 @@
+package kh.gov.camdx.disig.wrapperservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WrapperServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WrapperServiceApplication.class, args);
+	}
+
+}
---
+++ wrapper-service/src/test/java/kh/gov/camdx/disig/wrapperservice/WrapperServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package kh.gov.camdx.disig.wrapperservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WrapperServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
